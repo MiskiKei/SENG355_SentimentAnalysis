c08e10780dd9b25ef66141ef9c48676ee94e5b20
MiskiKei <107701715+MiskiKei@users.noreply.github.com>
2025-03-04 16:01:28 -0500

    Updated Connection and Get Post System

---
+++ reddit_api_connection.py
@@ -0,0 +1,27 @@
+# reddit_api.py
+import praw
+from config import CLIENT_ID, CLIENT_SECRET, USER_AGENT
+
+
+class RedditAPI:
+    def __init__(self):
+        self.reddit = praw.Reddit(
+            client_id=CLIENT_ID,
+            client_secret=CLIENT_SECRET,
+            user_agent=USER_AGENT
+        )
+
+    def get_posts(self, subreddit_name, limit=10):
+        subreddit = self.reddit.subreddit(subreddit_name)
+        return [submission.title + " " + submission.selftext for submission in subreddit.hot(limit=limit)]
+
+
+if __name__ == "__main__":
+    subreddit_name = "music"
+
+    reddit_api = RedditAPI()
+
+    posts = reddit_api.get_posts(subreddit_name)
+
+    for i, post in enumerate(posts):
+        print(f"{i + 1}: {post}\n")
